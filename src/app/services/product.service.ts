d37ae6ca621bf88b6421dac460c51eb7fcc4af3b
Evaldas Visockas <vievaldas@gmail.com>
2023-02-09 08:02:22 +0200

    Add basic auth to requests

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {Product} from "../models/product.model";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({providedIn: 'root'})
 export class ProductSuggestionService {
@@ -9,19 +9,27 @@ export class ProductSuggestionService {
 
   productSuggestionsUrl = 'http://localhost:8080/suggestions/';
   productCreationUrl = 'http://localhost:8080/product/create/';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + btoa('tester:tester')
+    })
+  };
+
   constructor(private http: HttpClient) {
 
   }
 
   getSuggestions(answer: { ageRange: any; studying: any; incomeRange: any; }) {
-    let result = this.http.post<Product[]>(this.productSuggestionsUrl, answer);
+    let result = this.http.post<Product[]>(this.productSuggestionsUrl, answer, this.httpOptions);
     result.subscribe(products => this.suggestions = products);
     return result;
   }
 
   createProduct(answer: { product: {name: any},
     answer: {ageRange: any; studying: any; incomeRange: any; }}) {
-    return this.http.post(this.productCreationUrl, answer);
+    return this.http.post(this.productCreationUrl, answer, this.httpOptions);
   }
 
 }
